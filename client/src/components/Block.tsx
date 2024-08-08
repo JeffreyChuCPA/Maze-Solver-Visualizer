ae5ab02da14b62b877242156070f6d0bf0be9339
Jeffrey Chu <jeffcolinchu@gmail.com>
2024-08-08 00:22:12 -0400

    small changes made on top of colorSelect branch

--- client/src/components/Block.tsx
+++ client/src/components/Block.tsx
@@ -2,17 +2,16 @@ import React, { useContext } from "react";
 import "../styling/Maze.css";
 import { PageContext } from "../PageProvider";
 import { Maze, Point } from "../utilities/types";
-import { isValidBoardPoint, isValidEndPoint, isValidStartPoint, resetStartOrEndPoint, switchBlockType, updateMaze } from "../utilities/utilities";
+import { isValidBoardPoint, isValidEndPoint, isValidStartPoint, resetStartOrEndPoint, switchBlockType } from "../utilities/utilities";
 import { MazeContext } from "../MazeProvider";
 
 type BlockProps = {
   blockType: number;
-  maze: Maze;
   rowIndex: number;
   colIndex: number;
 };
 
-const Block: React.FC<BlockProps> = ({ blockType, maze, rowIndex, colIndex }) => {
+const Block: React.FC<BlockProps> = ({ blockType, rowIndex, colIndex }) => {
   const {currentPage} = useContext(PageContext)
   const {mazeSize, setMaze, pathColor, wallColor, walkedColor, queuedColor, shortPathColor} = useContext(MazeContext)
   const cell: Point = {x: rowIndex, y: colIndex}
@@ -52,7 +51,7 @@ const Block: React.FC<BlockProps> = ({ blockType, maze, rowIndex, colIndex }) =>
         newMaze[cell.x][cell.y] = 0
       }
       //*Set wall point within board
-      if (isValidBoardPoint(cell.x, cell.y, newMaze)) {
+      else if (isValidBoardPoint(cell.x, cell.y, newMaze)) {
         const cellValue: number = switchBlockType(newMaze[cell.x][cell.y])
         newMaze[cell.x][cell.y] = cellValue
       }
